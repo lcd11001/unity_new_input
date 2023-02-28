c9f7e135477aafa897e237ded12cd6741f38ecd5
LCD-PC <dan.luongcong@gameloft.com>
2023-02-28 07:45:35 +0700

    + touch position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,15 @@ using UnityEngine.InputSystem.Interactions;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Camera cam;
+    [SerializeField] Transform debugSphere;
     [SerializeField] float moveSpeed;
     [SerializeField] float rotateSpeed;
 
     [SerializeField] GameObject Tap, Hold, HoldReleased, HoldReleasedSuccess, HoldReleasedCancel;
 
     Vector2 _movement;
+    Vector3 _touchPosition;
 
 #if ENABLE_INPUT_SYSTEM
     // This method is called by PlayerInput with behavior SendMessages
@@ -61,6 +64,22 @@ public class PlayerController : MonoBehaviour
                 break;
         }
     }
+
+    public void OnTouchPosition(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Canceled:
+            case InputActionPhase.Performed:
+                TouchPositionInput(context.ReadValue<Vector2>());
+                break;
+            //case InputActionPhase.Canceled:
+            //    TouchPositionInput(Vector3.zero);
+            //    break;
+            default:
+                break;
+        }
+    }
 #endif
 
     private void SetAllInactive()
@@ -77,6 +96,16 @@ public class PlayerController : MonoBehaviour
         _movement = newMove;
     }
 
+    private void TouchPositionInput(Vector3 newPos)
+    {
+        Ray ray = cam.ScreenPointToRay(newPos);
+        int layer = 1 << LayerMask.NameToLayer("Ground");
+        if (Physics.Raycast(ray, out RaycastHit info, 100f, layer))
+        {
+            _touchPosition = info.point;
+        }
+    }
+
     private void Awake()
     {
         SetAllInactive();
@@ -90,6 +119,19 @@ public class PlayerController : MonoBehaviour
             float targetAngle = Mathf.Atan2(_movement.x, _movement.y) * Mathf.Rad2Deg;
             float angle = Mathf.LerpAngle(transform.rotation.eulerAngles.y, targetAngle, rotateSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+
+        if (_touchPosition != Vector3.zero)
+        {
+            Vector3 facingDirection = _touchPosition - transform.position;
+            float targetAngle = Mathf.Atan2(facingDirection.x, facingDirection.z) * Mathf.Rad2Deg;
+            float angle = Mathf.LerpAngle(transform.rotation.eulerAngles.y, targetAngle, rotateSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        }
+
+        if (debugSphere != null)
+        {
+            debugSphere.position = _touchPosition;
         }
     }
 }
