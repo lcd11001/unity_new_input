786ee5ab39ca2c3b9f0a2224f9ab1a5739872001
LCD-PC <dan.luongcong@gameloft.com>
2023-02-28 11:11:13 +0700

    * sync logic between mouse / keyboard

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -131,12 +131,13 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        transform.position += new Vector3(_movement.x, 0, _movement.y) * moveSpeed * Time.deltaTime;
         if (_movement != Vector2.zero)
         {
             float targetAngle = Mathf.Atan2(_movement.x, _movement.y) * Mathf.Rad2Deg;
             float angle = Mathf.LerpAngle(transform.rotation.eulerAngles.y, targetAngle, rotateSpeed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        
+            transform.position += new Vector3(_movement.x, 0, _movement.y) * moveSpeed * Time.deltaTime;
         }
 
         if (isTouchPressed)
